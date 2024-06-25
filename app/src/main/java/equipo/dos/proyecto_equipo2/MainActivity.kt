2861646af4dfb46d6ef06ba678fd0631df34d936
waw89 <lcontrerasperaza99@gmail.com>
2024-06-25 09:41:40 -0700

    Avance en registro de actividad

--- app/src/main/java/equipo/dos/proyecto_equipo2/MainActivity.kt
+++ app/src/main/java/equipo/dos/proyecto_equipo2/MainActivity.kt
@@ -1,5 +1,6 @@
-    package equipo.dos.proyecto_equipo2
+package equipo.dos.proyecto_equipo2
 
+import HomeActivity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -9,8 +10,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,37 +18,32 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        var etEmail: EditText = findViewById(R.id.etEmail)
-        var etPassword: EditText = findViewById(R.id.etPassword)
-        var cbRemember: CheckBox = findViewById(R.id.checkRemember)
-        var btnLogin: Button = findViewById(R.id.btnLogIn)
-        var tvSignUp: TextView = findViewById(R.id.linkSignUp)
-
-        var email: String = etEmail.text.toString()
-        var password: String = etPassword.text.toString()
-
-
+        val etEmail: EditText = findViewById(R.id.etEmail)
+        val etPassword: EditText = findViewById(R.id.etPassword)
+        val cbRemember: CheckBox = findViewById(R.id.checkRemember)
+        val btnLogin: Button = findViewById(R.id.btnLogIn)
+        val tvSignUp: TextView = findViewById(R.id.linkSignUp)
 
         btnLogin.setOnClickListener {
+            val email: String = etEmail.text.toString()
+            val password: String = etPassword.text.toString()
 
-            if(validarCredenciales(email, password)){
-                var intent = Intent(this, HomeActivity::class.java)
-                this.startActivity(intent)
-            }else{
+            if (validarCredenciales(email, password)) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            } else {
                 Toast.makeText(this, "Email o contraseña incorrectos", Toast.LENGTH_SHORT).show()
             }
-
         }
 
         tvSignUp.setOnClickListener {
-            var intent = Intent(this, Register_Activity::class.java)
-            this.startActivity(intent)
+            val intent = Intent(this, Register_Activity::class.java)
+            startActivity(intent)
         }
-
     }
-    fun validarCredenciales(email: String, password: String): Boolean{
+
+    fun validarCredenciales(email: String, password: String): Boolean {
         // Pendiente para el backEnd
         return true
     }
-
 }
